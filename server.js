09635f883ec961d94131e822cf3486990a96e680
Dr. Vortex <jp@drvortex.dev>
2022-03-08 10:24:17 -0500

    Updated server.js and added index.html

--- server.js
+++ server.js
@@ -7,26 +7,13 @@ app
 .get('/packets', (req, res) => {
     res.send(`recieved ${packet} packets`);
 })
-.get('/libraries/jquery.js', (req, res) => {res.sendFile('d:/annihilation-multiplayer/libraries/jquery.js')})
-.get('/libraries/socketio.js', (req, res) => {res.sendFile('d:/annihilation-multiplayer/libraries/socketio.js')})
-.get('/',(req,res) => {
-	res.send(`
-		<html>
-			<head>
-				<link rel=stylesheet href=https://a.drvortex.dev/libraries/styles.css>
-			</head>
-			<script src=/libraries/jquery.js></script>
-			<script src=/libraries/socketio.js></script>
-			<body>
-				<button></button>
-			</body>
-		</html>
-	`);
-})
+.get('/libraries/jquery.js', (req, res) => {res.sendFile('./libraries/jquery.js')})
+.get('/libraries/socketio.js', (req, res) => {res.sendFile('./libraries/socketio.js')})
+.get('/',(req,res) => {res.sendFile('./index.html')});
 
 io.on('connection', socket => {
     socket.emit('packet','client connected');
-	console.log('client connected')
+	console.log('client connected');
     socket.on('packet', (...args) => {
         socket.emit('packet', `packet #${++packet} recieved`);
     });
