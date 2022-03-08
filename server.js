1bc0dd5846c2152a075c886793c2dd604850df01
Dr. Vortex <jp@drvortex.dev>
2022-03-07 23:12:16 -0500

    Added server.js

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
-const io = require('socket.io');
+const io = require('socket.io'), express = require('express');
 
-const socket = new Server(3000);
+const socket = new Server(3000), app = express();
 
 let packet = 0;
+app.get('/', (req, res) => {
+    res.send(`recieved ${packet} packets`);
+}).listen(80)
 
 io.on('connection', socket => {
     socket.emit('packet','client connected');
-    let packet = 0;
     socket.on('packet', (...args) => {
         socket.emit('packet', `packet #${++packet} recieved`);
     });
