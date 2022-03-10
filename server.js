9a5861d30e94630a9de5e9fe63076d281eed248e
Dr. Vortex <jp@drvortex.dev>
2022-03-10 14:06:46 -0500

    Update: added chat and decreased how much information is logged

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const get = (url, callback) => {
     });
 };
 
-let packets = 0, pings = 0, sent = 0, log = [], clients = {};
+let log = [], chat = [], clients = {};
 
 app
 .get('/libraries/jquery.js', (req, res) => {res.sendFile('./libraries/jquery.js')})
@@ -24,19 +24,14 @@ app
 .get('/',(req,res) => {res.sendFile('./index.html')});
 
 io.on('connection', socket => {
-    socket.emit('packet','client connected');
     clients[socket.id] = Object.assign(socket,{username:'dummy',uuid:-1,oplvl:-1,authorized: false});
-    sent++;
     log.push('client connected with id '+socket.id);
 	socket.on('ping', data => {
 	    log.push('recieved ping');
-	    pings++;
 	    socket.emit('packet', {clients: io.engine.clientsCount, status: 'online'});
-	    sent++;
 	});
 	socket.on('packet', (type, data) => {
 	    log.push('recieved packet: '+type);
-	    packets++;
 	    switch(type){
 	        case 'get-clients':
 	            if(socket.authorized){
@@ -50,11 +45,9 @@ io.on('connection', socket => {
 	                        authorized: clients[id].authoized
 	                    });
 	                }
-	                log.push('sent packet: clients');
 	                socket.emit('packet', res);
 	                break;
 	            }else{
-	                log.push('sent packet: not authorized');
 	                socket.emit('packet', 'Not authorized, please send an "auth" packet with your token');
 	            }
                 break;
@@ -71,19 +64,19 @@ io.on('connection', socket => {
                         user = JSON.parse(res);
                         log.push(`client ${socket.id} authorized as ${user.id} (${user.username})`);
                         Object.assign(clients[socket.id], {uuid: user.id, username: user.username, oplvl: parseFloat(user.oplvl), authorized: true});
-                        log.push('sent packet: auth success');
                         socket.emit('packet','authorized as '+user.username);
                     }else{
-                        log.push('sent packet: auth failed');
                         socket.emit('packet','authorization failed');
                     }
                 });
                 break;
+            case 'chat':
+                log.push(`[Chat] ${socket.username}: ${data}`);
+                io.emit('chat',{user: socket.username,  content: data});
+                break;
             default:
-                log.push('sent packet: invalid packet');
                 socket.emit('packet',`"${type}" packet not accepted`);
 	    }
-	    sent++;
 	});
 });
 server.listen(80, e => log.push('server started'));
