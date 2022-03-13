70965954f6a801bfd4f6467b69db561087046c30
Dr. Vortex <jp@drvortex.dev>
2022-03-12 19:41:26 -0500

    Implemented whitelist/blacklist checking

--- server.js
+++ server.js
@@ -2,11 +2,10 @@
 const
 https = require('https'), fs = require('fs'), ini = require('ini'),
 server = require('http').createServer((req, res) => {
-    if(req.url == '/ping'){
-        log('recieved ping');
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.end(JSON.stringify({currentPlayers: io.sockets.sockets.size, maxPlayers: config.max_players, status: 'online', message: config.message}));
-    }
+	if(req.url == '/ping'){
+		res.setHeader('Access-Control-Allow-Origin', '*');
+		res.end(JSON.stringify({currentPlayers: io.sockets.sockets.size, maxPlayers: config.max_players, status: 'online', message: config.message}));
+	}
 }),
 io = new (require('socket.io').Server)(server, { cors : {origin: 'https://annihilation.drvortex.dev'}});
 //BABYLON = require('babylonjs');
@@ -22,7 +21,7 @@ const get = (url, callback) => {
 			body += data;
 		});
 		res.on('end', e => {
-		   callback(body); 
+			callback(body); 
 		});
 	});
 },
@@ -35,8 +34,8 @@ isJSON = str => {
 	}
 },
 log = message => {
-    logs.push(message);
-    console.log(message);
+	logs.push(message);
+	console.log(message);
 };
 //global variables
 
@@ -49,20 +48,20 @@ const config = ini.parse(fs.readFileSync('./config.ini', 'utf-8'));
 let opsString = fs.readFileSync('./ops.json','utf-8');
 if(isJSON(opsString)){ops = JSON.parse(opsString)}
 if(config.whitelist){
-    let whitelistString = fs.readFileSync('./whitelist.json', 'utf-8');
-    if(isJSON(whitelistString)){
-        whitelist = JSON.parse(whitelistString);
-    }else{
-        log('Error reading whitelist: not JSON');
-    }
+	let whitelistString = fs.readFileSync('./whitelist.json', 'utf-8');
+	if(isJSON(whitelistString)){
+		whitelist = JSON.parse(whitelistString);
+	}else{
+		log('Error reading whitelist: not JSON');
+	}
 }
 if(config.blacklist){
-    let blacklistString = fs.readFileSync('./blacklist.json', 'utf-8');
-    if(isJSON(blacklistString)){
-        blacklist = JSON.parse(blacklistString);
-    }else{
-        log('Error reading blacklist: not JSON');
-    }
+	let blacklistString = fs.readFileSync('./blacklist.json', 'utf-8');
+	if(isJSON(blacklistString)){
+		blacklist = JSON.parse(blacklistString);
+	}else{
+		log('Error reading blacklist: not JSON');
+	}
 }
 
 //Babylon stuff
@@ -75,52 +74,51 @@ io.use((socket, next) => {
 			user = JSON.parse(res);
 			log(`${user.username} (${user.id}) connected with id ${socket.id}`);
 			players[socket.id] = socket.user = {socket: socket, id: user.id, username: user.username, oplvl: parseFloat(user.oplvl)};
+			if(io.sockets.sockets.size >= config.max_players && (ops[socket.user.id] && !ops[socket.user.id].bypassLimit)){
+				next(new Error('Connection refused: server full'));
+			}
+			if(config.whitelist && !whitelist.includes(user.id)){
+				next(new Error('Connection refused: you are not whitelisted'));
+			}
+			if(config.blacklist && blacklist.includes(user.id)){
+				next(new Error('Connection refused: you are banned from this server'));
+			}
 			next();
 		}else{
 			next(new Error('Connection refused: invalid token'));
 		}
 	});
 });
-io.use((socket, next) => {
-    if(io.sockets.sockets.size >= config.max_players && (ops[socket.user.id] && !ops[socket.user.id].bypassLimit)){
-        next(new Error('Connection refused: server full'));
-    }
-    next();
-});
 io.on('connection', socket => {
 	socket.on('disconnect', reason => {
-	   log(socket.user.username + ' disconnected: ' + reason);
-	   delete players[socket.id];
+		log(socket.user.username + ' disconnected: ' + reason);
+		delete players[socket.id];
 	});
-	socket.on('packet', (type, data) => {
-		log('recieved packet: '+type);
-		switch(type){
-			case 'get-clients':
-				let res = [];
-				for(let id in players){
-					res.push({
-						socket: id,
-						id: players[id].id,
-						username: players[id].username,
-						oplvl: players[id].oplvl
-					});
-				}
-				socket.emit('packet', res);
-				break;
-			case 'get-log':
-				if(ops[socket.user.id] && ops[socket.user.id].level > 0){
-					socket.emit('packet', logs);
-				}else{
-					socket.emit('packet', 'Forbidden');
-				}
-				break;
-			case 'chat':
-				log(`[Chat] ${socket.user.username}: ${data}`);
-				io.emit('chat',{user: socket.user.username,  content: data});
-				break;
-			default:
-				socket.emit('packet',`"${type}" packet not accepted`);
+	socket.onAny(type => {
+		 log('recieved packet: ' + type);
+	});
+	socket.on('get-clients', data => {
+		let res = [];
+		for(let id in players){
+			res.push({
+				socket: id,
+				id: players[id].id,
+				username: players[id].username,
+				oplvl: players[id].oplvl
+			});
 		}
+		socket.emit('packet', res);
+	});
+	socket.on('get-log', data => {
+		if(ops[socket.user.id] && ops[socket.user.id].level > 0){
+			socket.emit('packet', logs);
+		}else{
+			socket.emit('packet', 'Forbidden');
+		} 
+	});
+	socket.on('chat', data => {
+		log(`[Chat] ${socket.user.username}: ${data}`);
+		io.emit('chat',{user: socket.user.username, content: data});
 	});
 });
 server.listen(80, e => log('server started'));
